fcf4212fafbd90f5b4135a17a5394f204b1506d7
Dave <dave.cadelina@outlook.com>
2021-09-15 01:49:15 +1000

    found genres. Add next

--- src/components/film/film.component.jsx
+++ src/components/film/film.component.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import './film.styles.scss'
 
 const Film = ({film}) => {
-    console.log("film", film)
     return (
         <div className="film__container">
             <img src={`https://image.tmdb.org/t/p/original${film.poster_path}`} alt="" />
--- src/redux/film/film.sagas.js
+++ src/redux/film/film.sagas.js
@@ -2,22 +2,32 @@ import { takeLatest, put, all, call, } from "redux-saga/effects";
 import { filmActionTypes } from './film.types'
 import { setBannerImage, setTrending, setPopular } from './film.actions'
 
+//-- GENRE's
+//-- https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=28
+
 const apiKey = '08aabbbef104512bb5432031efeae18c'
 
 const requests = {
     discover: `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_providers=netflix&with_watch_monetization_types=flatrate`,
-    trending: `https://api.themoviedb.org/3/trending/movie/day?api_key=${apiKey}`
+    trending: `https://api.themoviedb.org/3/trending/movie/day?api_key=${apiKey}`,
+    popular: `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`
 }
 
 export function* fetchDataStartAsync() {
     try {
         const randomNumber = Math.floor(Math.random() * 20)
+        let fetchPopular = yield fetch(requests.popular)
+        let popular = yield fetchPopular.json()
+        let bannerImage = `https://image.tmdb.org/t/p/original${popular.results[randomNumber].backdrop_path}`
+        
         let fetchTrending = yield fetch(requests.trending)
         let trending = yield fetchTrending.json()
 
-        let fetchPopular = yield fetch(requests.discover)
-        let popular = yield fetchPopular.json()
-        let bannerImage = `https://image.tmdb.org/t/p/original${popular.results[randomNumber].backdrop_path}`
+
+        let fetchGenre = yield fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=28`)
+        let genre = yield fetchGenre.json()
+        console.log("genre", genre)
+
         yield put(setBannerImage(bannerImage))
         yield put(setTrending(trending.results))
         yield put(setPopular(popular.results))
